81432752b6d22c2a516e45f6379d1999ec000003
Seraffy <gabrielsantos.s2018@outlook.com>
2023-05-27 14:11:37 -0300

    agora só falta o find products no index.html

--- pesquisa.js
+++ pesquisa.js
@@ -1,2 +1,27 @@
 
-var barraPesquisa = document.getElementById("barrapesquisa")
+      
+const barraPesquisa = document.getElementById("search-bar")
+const cards = document.querySelectorAll(".card-number");
+
+
+barraPesquisa.addEventListener('input',pesquisa);
+
+ function pesquisa(){
+   
+    if(barraPesquisa != ''){
+        for(let card of cards){
+            let title = card.querySelector('h5');
+            title = title.textContent.toLowerCase();
+            console.log(title);
+            let filtro = barraPesquisa.value.toLowerCase();
+
+            if(!title.includes(filtro)){
+                card.style.display =  "none";
+            }
+            else {
+                card.style.display = "block";
+            }
+        }
+    }
+}
+
